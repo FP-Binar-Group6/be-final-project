abc947dcbb8348caa2b98530ad486268d3acd64a
Abid Bilal <abiddddbilal@gmail.com>
2023-06-06 22:15:04 +0700

    initial commit

---
+++ src/test/java/com/fpbinar6/code/CodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpbinar6.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
