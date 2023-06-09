35245299fd983a28e6554e5cde9db128e9df85bf
AlfaKazama <alfakazamaa@gmail.com>
2023-06-09 20:21:26 +0700

    Menambahkan DeleteMapping dan PutMapping

--- src/main/java/com/fpbinar6/code/controllers/AirlineController.java
+++ src/main/java/com/fpbinar6/code/controllers/AirlineController.java
@@ -3,10 +3,7 @@ package com.fpbinar6.code.controllers;
 import com.fpbinar6.code.models.Airline;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.fpbinar6.code.services.AirlineService;
 import com.fpbinar6.code.utils.Constants;
@@ -29,6 +26,18 @@ public class AirlineController {
     @GetMapping("/{id}")
     public ResponseEntity<Object> getAirlineById(@PathVariable("id") int id) {
         var airline = airlineService.getAirlineById(id);
+        return ResponseHandler.generateResponse(Constants.SUCCESS_RETRIEVE_MSG, HttpStatus.OK, airline);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deteletAirlineById(@PathVariable("id") int id){
+        airlineService.deleteAirlineById(id);
+        return ResponseHandler.generateResponse(Constants.SUCCESS_RETRIEVE_MSG, HttpStatus.OK, id);
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<Object> updateAirline(@RequestBody Airline airline){
+        airlineService.updateAirline(airline);
         return ResponseHandler.generateResponse(Constants.SUCCESS_RETRIEVE_MSG, HttpStatus.OK, airline);
     }
 }
