bc1024af5d89b9594f1cb14c6ac5886f35f6bbf6
Abid Bilal <abiddddbilal@gmail.com>
2023-07-05 00:20:14 +0700

    fix seat

--- src/main/java/com/fpbinar6/code/seeder/SeatSeeder.java
+++ src/main/java/com/fpbinar6/code/seeder/SeatSeeder.java
@@ -20,7 +20,7 @@ import com.fpbinar6.code.models.dto.SeatSeederDTO;
 import com.fpbinar6.code.repository.ScheduleRepository;
 import com.fpbinar6.code.repository.SeatRepository;
 
-@Component
+
 @Order(1)
 public class SeatSeeder implements CommandLineRunner {
 
